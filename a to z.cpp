a71e65b8373b7a7a9e69ca7a9a8adecaf4a59150
sehar <57066970+sehar2807@users.noreply.github.com>
2019-11-09 21:57:30 +0500

    Add files via upload

---
+++ a to z.cpp
@@ -0,0 +1,384 @@
+//program to display anything with ** :)
+#include<iostream>
+#include<conio.h>
+#include<windows.h>
+using namespace std;
+int main()
+{
+	char ch[20]={0};
+	cout<<"Enter What You Want :) ";
+	gets (ch);
+	    cout<<"\n\n\n";
+	    
+   for(int jk=0;jk<=19;jk++)
+   {
+	//check for a
+	
+	if(ch[jk]=='A'|| ch[jk]=='a')
+	{
+		cout<<"\n\t\t\t       *";
+		cout<<"\n\t\t\t     *   *";
+		cout<<"\n\t\t\t    *     *";
+		cout<<"\n\t\t\t   *       *";
+		cout<<"\n\t\t\t  *  * * *  *";
+		cout<<"\n\t\t\t *           *";
+		cout<<"\n\t\t\t*             *";
+		cout<<"\n\n";
+	}
+	//check for b
+	
+	
+     else if(ch[jk]=='B'|| ch[jk]=='b')
+	{
+		cout<<"\n\t\t\t * * * *";
+		cout<<"\n\t\t\t *       *";
+		cout<<"\n\t\t\t *      *";
+		cout<<"\n\t\t\t * * *";
+		cout<<"\n\t\t\t *      *";
+		cout<<"\n\t\t\t *       *";
+		cout<<"\n\t\t\t * * * *";
+		cout<<"\n\n";
+	}
+	
+	//check for c
+	
+	if(ch[jk]=='C'||ch[jk]=='c')
+	{
+		cout<<"\n\t\t\t    * * * *  ";
+		cout<<"\n\t\t\t  *          ";
+		cout<<"\n\t\t\t  *          ";
+		cout<<"\n\t\t\t  *          ";
+		cout<<"\n\t\t\t  *          ";
+		cout<<"\n\t\t\t  *          ";
+		cout<<"\n\t\t\t  * * * * *  ";
+		cout<<"\n\n";
+	}
+	
+	//check for d
+	
+	if(ch[jk]=='D'||ch[jk]=='d')
+	{
+		cout<<"\n\t\t\t * * * *"   ;
+		cout<<"\n\t\t\t *       *" ;
+		cout<<"\n\t\t\t *        *";
+		cout<<"\n\t\t\t *        *";
+		cout<<"\n\t\t\t *        *";
+		cout<<"\n\t\t\t *       *" ;
+		cout<<"\n\t\t\t * * * *"   ;
+		cout<<"\n\n";
+	}
+	
+	//check for e
+	
+	if(ch[jk]=='E'||ch[jk]=='e')
+	{
+		cout<<"\n\t\t\t * * * * *   " ;
+		cout<<"\n\t\t\t *           " ;
+		cout<<"\n\t\t\t *           " ;
+		cout<<"\n\t\t\t * * * * *   " ;
+		cout<<"\n\t\t\t *           " ;
+		cout<<"\n\t\t\t *           " ;
+		cout<<"\n\t\t\t * * * * *   " ;	
+		cout<<"\n\n";
+	}
+	
+	//check for f
+	
+	if(ch[jk]=='F'||ch[jk]=='f')
+	{
+		cout<<"\n\t\t\t * * * * *   " ;
+		cout<<"\n\t\t\t *           " ;
+		cout<<"\n\t\t\t *           " ;
+		cout<<"\n\t\t\t * * * * *   " ;
+		cout<<"\n\t\t\t *           " ;
+		cout<<"\n\t\t\t *           " ;
+		cout<<"\n\t\t\t *           " ;	
+		cout<<"\n\n";
+	}
+	
+	//check for g
+	
+	 if(ch[jk]=='G'||ch[jk]=='g')
+	{
+		cout<<"\n\t\t\t    * * * * *   ";
+		cout<<"\n\t\t\t  *            ";
+		cout<<"\n\t\t\t  *            ";
+		cout<<"\n\t\t\t  *            ";
+		cout<<"\n\t\t\t  *    * * * * ";
+		cout<<"\n\t\t\t  *    *     * ";
+		cout<<"\n\t\t\t  * * * * * *  ";
+		cout<<"\n\n";
+	}
+	
+	//check for h
+	
+    if (ch[jk]=='h'|| ch[jk]=='H')
+	{
+		cout<<"\n\t\t\t *         *  " ;
+		cout<<"\n\t\t\t *         *  " ;
+		cout<<"\n\t\t\t *         *  " ;
+		cout<<"\n\t\t\t * * * * * *  " ;
+		cout<<"\n\t\t\t *         *  " ;
+		cout<<"\n\t\t\t *         *  " ;
+		cout<<"\n\t\t\t *         *  " ;	
+		cout<<"\n\n";
+	}
+	
+	//check for i
+	
+    if (ch[jk]=='I' || ch[jk]=='i')
+		{
+		cout<<"\n\t\t\t * * * * * * *     " ;
+		cout<<"\n\t\t\t       *           " ;
+		cout<<"\n\t\t\t       *           " ;
+		cout<<"\n\t\t\t       *           " ;
+		cout<<"\n\t\t\t       *           " ;
+		cout<<"\n\t\t\t       *           " ;
+		cout<<"\n\t\t\t * * * * * * *     " ;	
+		cout<<"\n\n";
+	    }
+	
+	
+    	//check for j
+	
+	 if (ch[jk]=='J' || ch[jk]=='j')
+		{
+		cout<<"\n\t\t\t    * * * * * * *     " ;
+		cout<<"\n\t\t\t          *           " ;
+		cout<<"\n\t\t\t          *           " ;
+		cout<<"\n\t\t\t          *           " ;
+		cout<<"\n\t\t\t *        *           " ;
+		cout<<"\n\t\t\t *        *           " ;
+		cout<<"\n\t\t\t  * * *  *            " ;	
+		cout<<"\n\n";
+	    }
+	
+		//check for k
+	
+	 if (ch[jk]=='K' || ch[jk]=='k')
+		{
+		cout<<"\n\t\t\t      *       *      " ;
+		cout<<"\n\t\t\t      *     *        " ;
+		cout<<"\n\t\t\t      *   *          " ;
+		cout<<"\n\t\t\t      *  *           " ;
+		cout<<"\n\t\t\t      *    *         " ;
+		cout<<"\n\t\t\t      *      *       " ;
+		cout<<"\n\t\t\t      *        *     " ;	
+		cout<<"\n\n";
+	}
+
+	//check for L
+	
+    if (ch[jk]=='L' || ch[jk]=='l')
+		{
+		cout<<"\n\t\t\t       *              " ;
+		cout<<"\n\t\t\t       *              " ;
+		cout<<"\n\t\t\t       *              " ;
+		cout<<"\n\t\t\t       *              " ;
+		cout<<"\n\t\t\t       *              " ;
+		cout<<"\n\t\t\t       *              " ;
+		cout<<"\n\t\t\t       * *  * * * *   " ;	
+		cout<<"\n\n";
+	}
+
+	//check for m
+	
+	if(ch[jk]=='m'|| ch[jk]=='M')
+	{
+		cout<<"\n\t\t\t   *         *          ";
+		cout<<"\n\t\t\t   * *     * *         ";
+		cout<<"\n\t\t\t   *  *   *  *        ";
+		cout<<"\n\t\t\t   *   * *   *       ";
+		cout<<"\n\t\t\t   *    *    *      ";
+		cout<<"\n\t\t\t   *         *     ";
+		cout<<"\n\t\t\t   *         *    ";
+		cout<<"\n\n";
+	}
+
+	//check for n
+	
+	if(ch[jk]=='N'|| ch[jk]=='n')
+	{
+		cout<<"\n\t\t\t  *           *  ";
+		cout<<"\n\t\t\t  * *         *   ";
+		cout<<"\n\t\t\t  *   *       *    ";
+		cout<<"\n\t\t\t  *     *     *     ";
+		cout<<"\n\t\t\t  *       *   *      ";
+		cout<<"\n\t\t\t  *         * *       ";
+		cout<<"\n\t\t\t  *           *        ";
+		cout<<"\n\n";
+	}
+	
+		//check for o
+	
+	if(ch[jk]=='O'||ch[jk]=='o')
+	{
+		cout<<"\n\t\t\t    * * * *     ";
+		cout<<"\n\t\t\t  *         *   ";
+		cout<<"\n\t\t\t  *         *   ";
+		cout<<"\n\t\t\t  *         *   ";
+		cout<<"\n\t\t\t  *         *   ";
+		cout<<"\n\t\t\t  *         *   ";
+		cout<<"\n\t\t\t    * * * *     ";
+		cout<<"\n\n";
+	}
+	
+	//check for p
+	
+	
+     if(ch[jk]=='P'|| ch[jk]=='p')
+	{
+		cout<<"\n\t\t\t  * * * *   ";
+		cout<<"\n\t\t\t *       *  ";
+		cout<<"\n\t\t\t *       *  ";
+		cout<<"\n\t\t\t * * * *    ";
+		cout<<"\n\t\t\t *          ";
+		cout<<"\n\t\t\t *          ";
+		cout<<"\n\t\t\t *          ";
+		cout<<"\n\n";
+	}
+		
+	//check for q
+	
+	if(ch[jk]=='Q'||ch[jk]=='q')
+	{
+		cout<<"\n\t\t\t    * * * *     ";
+		cout<<"\n\t\t\t  *         *   ";
+		cout<<"\n\t\t\t  *         *   ";
+		cout<<"\n\t\t\t  *         *   ";
+		cout<<"\n\t\t\t  *     *   *   ";
+		cout<<"\n\t\t\t  *       * *   ";
+		cout<<"\n\t\t\t    * * * * *   ";
+		cout<<"\n\t\t\t              * ";
+		cout<<"\n\n";
+	}
+	
+		//check for r
+	
+	
+     if(ch[jk]=='R'|| ch[jk]=='r')
+	{
+		cout<<"\n\t\t\t * * * * *   ";
+		cout<<"\n\t\t\t *        *  ";
+		cout<<"\n\t\t\t *        *  ";
+		cout<<"\n\t\t\t * * * * *    ";
+		cout<<"\n\t\t\t *  *       ";
+		cout<<"\n\t\t\t *    *      ";
+		cout<<"\n\t\t\t *       *   ";
+		cout<<"\n\n";
+	}
+	
+		//check for s
+	
+	
+     if(ch[jk]=='S'|| ch[jk]=='s')
+	{
+		cout<<"\n\t\t\t    * * *      ";
+		cout<<"\n\t\t\t  *            ";
+		cout<<"\n\t\t\t  *            ";
+		cout<<"\n\t\t\t    * * *      ";
+		cout<<"\n\t\t\t         *     ";
+		cout<<"\n\t\t\t         *     ";
+		cout<<"\n\t\t\t   * * *       ";
+		cout<<"\n\n";
+	}
+	//check for t
+	
+    if (ch[jk]=='T' || ch[jk]=='t')
+		{
+		cout<<"\n\t\t\t * * * * * * *     " ;
+		cout<<"\n\t\t\t       *           " ;
+		cout<<"\n\t\t\t       *           " ;
+		cout<<"\n\t\t\t       *           " ;
+		cout<<"\n\t\t\t       *           " ;
+		cout<<"\n\t\t\t       *           " ;
+		cout<<"\n\t\t\t       *           " ;	
+		cout<<"\n\n";
+	}	
+	
+			//check for u
+	
+	if(ch[jk]=='U'||ch[jk]=='u')
+	{
+		cout<<"\n\t\t\t  *         *   ";
+		cout<<"\n\t\t\t  *         *   ";
+		cout<<"\n\t\t\t  *         *   ";
+		cout<<"\n\t\t\t  *         *   ";
+		cout<<"\n\t\t\t  *         *   ";
+		cout<<"\n\t\t\t  *         *   ";
+		cout<<"\n\t\t\t    * * * *     ";
+		cout<<"\n\n";
+	}
+	
+		//check for v
+	
+	if(ch[jk]=='V'|| ch[jk]=='v')
+	{
+		cout<<"\n\t\t\t    *           *  ";
+		cout<<"\n\t\t\t     *         *   ";
+		cout<<"\n\t\t\t      *       *    ";
+		cout<<"\n\t\t\t       *     *     ";
+		cout<<"\n\t\t\t        *   *      ";
+		cout<<"\n\t\t\t         * *       ";
+		cout<<"\n\t\t\t          *        ";
+		cout<<"\n\n";
+	}
+
+	//check for w
+	
+	if(ch[jk]=='W'|| ch[jk]=='w')
+	{
+		cout<<"\n\t\t\t  *      *      *   ";
+		cout<<"\n\t\t\t  *     * *     *   ";
+		cout<<"\n\t\t\t  *    *   *    *   ";
+		cout<<"\n\t\t\t  *   *     *   *   ";
+		cout<<"\n\t\t\t  *  *       *  *   ";
+		cout<<"\n\t\t\t  * *         * *   ";
+		cout<<"\n\t\t\t  *             *    ";
+		cout<<"\n\n";
+	}
+	
+			//check for x
+	
+	 if (ch[jk]=='X' || ch[jk]=='x')
+		{
+		cout<<"\n\t\t\t   *           *      " ;
+		cout<<"\n\t\t\t     *       *        " ;
+		cout<<"\n\t\t\t       *   *          " ;
+		cout<<"\n\t\t\t         *           " ;
+		cout<<"\n\t\t\t       *   *         " ;
+		cout<<"\n\t\t\t     *       *       " ;
+		cout<<"\n\t\t\t   *           *     " ;	
+		cout<<"\n\n";
+	}
+
+	//check for y
+	
+	if (ch[jk]=='Y' || ch[jk]=='y')
+		{
+		cout<<"\n\t\t\t   *           *  " ;
+		cout<<"\n\t\t\t     *       *    " ;
+		cout<<"\n\t\t\t       *   *      " ;
+		cout<<"\n\t\t\t         *        " ;
+		cout<<"\n\t\t\t       *          " ;
+		cout<<"\n\t\t\t     *            " ;
+		cout<<"\n\t\t\t   *              " ;
+		cout<<"\n\n";	
+	}
+
+	//check for z
+	
+    if (ch[jk]=='z' || ch[jk]=='z')
+		{
+		cout<<"\n\t\t\t * * * * * * *    " ;
+		cout<<"\n\t\t\t           *      " ;
+		cout<<"\n\t\t\t         *        " ;
+		cout<<"\n\t\t\t       *          " ;
+		cout<<"\n\t\t\t     *            " ;
+		cout<<"\n\t\t\t   *              " ;
+		cout<<"\n\t\t\t * * * * * * *    " ;
+		cout<<"\n\n";	
+	}
+	
+   }//for 
+}//return main
